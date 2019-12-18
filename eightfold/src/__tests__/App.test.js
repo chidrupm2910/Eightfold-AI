cfabc7784d501396610ae8fecdafc0f4f72fe1f9
Chidrup <cmahakali@paypal.com>
2019-12-17 22:04:23 -0800

    Test Changes

--- eightfold/src/__tests__/App.test.js
+++ eightfold/src/__tests__/App.test.js
@@ -30,61 +30,59 @@ const response = {
         Response: 'true',
         Search: []
     },
-    
+
 };
 const mockStore = configureMockStore([thunk]);
 describe('MOVIE INPUT TESTS', () => {
-   
+
     let wrapper;
     const store = mockStore(initialState);
     beforeEach(() => {
-       
-
         wrapper = mount(<Provider store={store}><App /></Provider>);
     });
+
     afterEach(() => moxios.uninstall());
     it('Application should render without crashing', () => {
-        console.log(wrapper.debug());
         expect(wrapper).toBeTruthy();
     });
 
-    it('Should Render One Input Component', () => {
+    it('Should Render One Input Component for Movie Search', () => {
         const listItem = wrapper.find('.movie-dropdown input');
         expect(listItem.length).toBe(1);
-    });    it('Should Render One Input Component', () => {
-        const listItem = wrapper.find('.movie-dropdown input');
-        expect(listItem.length).toBe(1);
-    })
+    });
+
+    it('Should Dispatch the corresponding action when searched for a movie title', () => {
+        moxios.stubRequest('http://www.omdbapi.com/?apikey=818b96a1&s=shawshank%20redemption', {
+            status: 200,
+            response
+        });
+
+        const expectedAction = [{ type: actionTypes.UPDATE_MOVIE_TITLES_FROM_SEARCH, dropdownMovieList: response.data.Search, errorMsg: '' }];
+        store.dispatch(actions.movieNameSearch())
+            .then(() => {
+                expect(store.getActions()).toEqual(expectedAction);
+            });
+    });
+
+    it('Should Render One Movie Dropdown Component', () => {
+        const dropdown = wrapper.find('.movie-dropdown');
+        expect(dropdown.length).toBe(1);
+    });
 
     it('Should render the same number of Dropdown items as available in selected movies list', () => {
 
         const listItem = wrapper.find('.movie-bubble');
         expect(listItem.length).toBe(initialState.movies.selectedMovieList.length);
-    })
-
-    it('Should Dispatch the corresponding action when searched for a  movie title', () => {
-        moxios.stubRequest('http://www.omdbapi.com/?apikey=818b96a1&s=shawshank%20redemption', {
-            status: 200,
-            response
-          });
-
-          const expectedAction = [{ type: actionTypes.UPDATE_MOVIE_TITLES_FROM_SEARCH, dropdownMovieList: response.data.Search, errorMsg: '' }];
-          store.dispatch(actions.movieNameSearch())
-            .then(() => {
-              expect(store.getActions()).toEqual(expectedAction);
-            });
     });
 
-    it('Should Render Respective number of movie titles available in the reducer in dropdown', () => {
-        const listItem = wrapper.find('.list-item');
-        expect(listItem.length).toBe(initialState.movies.dropdownMovieList.length);
-    });
 
     it('Checking Dispatched actions in the component when a list item is clicked', () => {
         const movie = wrapper.find('.list-item').at(4);
         movie.simulate('click');
-        const expectedAction = [ { type: actionTypes.UPDATE_ERROR_MESSAGE,
-        errorMsg: 'Server is unavailable' },{ type: actionTypes.UPDATE_SELECTED_MOVIE_LIST, selectedMovieList: [...initialState.movies.selectedMovieList,'Movie 5']}]
+        const expectedAction = [{
+            type: actionTypes.UPDATE_ERROR_MESSAGE,
+            errorMsg: 'Server is unavailable'
+        }, { type: actionTypes.UPDATE_SELECTED_MOVIE_LIST, selectedMovieList: [...initialState.movies.selectedMovieList, 'Movie 5'] }]
         expect(store.getActions()).toEqual(expectedAction);
 
     });
@@ -93,7 +91,16 @@ describe('MOVIE INPUT TESTS', () => {
         const app = wrapper.find('.app');
         app.simulate('click');
         expect(store.getActions()[2]).toEqual({ type: 'UPDATE_DROPDOWN_STATE', dropdownOpen: false })
-    })
+    });
 
+    it('Should Render Respective number of movie titles available in the reducer in dropdown', () => {
+        const listItem = wrapper.find('.list-item');
+        expect(listItem.length).toBe(initialState.movies.dropdownMovieList.length);
+    });
+
+    it('Number  of posters displayed need to be same as number of List Items', () => {
+        const images = wrapper.find('.list-item img');
+        expect(images.length).toBe(initialState.movies.dropdownMovieList.length);
+    })
 
 });
